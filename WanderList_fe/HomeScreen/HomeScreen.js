32038a0bcb56f0658602d48548053efea41077b8
Tom Luxton <thomas.luxton@yahoo.com.au>
2020-08-27 10:25:11 +1000

    Added contaners

--- WanderList_fe/HomeScreen/HomeScreen.js
+++ WanderList_fe/HomeScreen/HomeScreen.js
@@ -4,8 +4,21 @@ import {View, TouchableOpacity, Text, StyleSheet, TextInput, Button} from 'react
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: '#ffffff'
+  },
+  topContainer: {
+    flex: 1,
+    backgroundColor: '#999999'
+  },
+  bodyContainer: {
+    flex: 1,
+    backgroundColor: '#000000'
+  },
+  bottomContainer: {
+    flex: 1,
+    backgroundColor: '#555555'
   },
   button: {
     alignItems: 'center',
@@ -33,36 +46,47 @@ class HomeScreen extends Component {
     render() {
         return (
                 <View style={styles.container}>
-                    <Text style={styles.title}>
-                        Hi User
-                    </Text>
-                    <Text style={styles.subtitle}>
-                        Where would you like to travel?
-                    </Text>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() =>
-                        this.props.navigation.navigate('NearbyActivities')
-                        }
-                    >
-                        <Text> Nearby Activities </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() =>
-                        this.props.navigation.navigate('WanderLists')
-                        }
-                    >
-                    <Text> Your Lists </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() =>
-                        this.props.navigation.navigate('Profile')
-                        }
-                    >
-                    <Text> Profile </Text>
-                    </TouchableOpacity>
+                      <View stlye = {styles.topContainer}>
+                          <Text style={styles.title}>
+                              Hi User
+                          </Text>
+                          <Text style={styles.subtitle}>
+                              Where would you like to travel?
+                          </Text>
+                          <TextInput style={styles.textInput} placeholder="Seach" onChangeText={text => this.setState({seachVal: text})}/>
+                      </View>
+                      <View style = {styles.bodyContainer}>
+                        <Text style={styles.subtitle}> Popular </Text>
+
+
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() =>
+                            this.props.navigation.navigate('NearbyActivities')
+                            }
+                        >
+                            <Text> Nearby Activities </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() =>
+                            this.props.navigation.navigate('WanderLists')
+                            }
+                        >
+                        <Text> Your Lists </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() =>
+                            this.props.navigation.navigate('Profile')
+                            }
+                        >
+                        <Text> Profile </Text>
+                        </TouchableOpacity>
+                      </View>
+                      <View stlye = {styles.bottomContainer}>
+                        <Text style={styles.subtitle}>Recently Viewed</Text>
+                      </View>
               </View>
         )
     }
